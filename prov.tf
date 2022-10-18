8c47211d709384945a6c068a5e781c1e43e27166
Shawn-Shifflett <104603593+Shawn-Shifflett@users.noreply.github.com>
2022-10-17 20:49:16 -0400

    CentOS Proj

---
+++ prov.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.22.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+provider "aws" {
+  region = var.region
+}
